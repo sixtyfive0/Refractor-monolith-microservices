59ce3e180da042c903537d253036db5d99ca8a31
sixtyfive0 <54398129+sixtyfive0@users.noreply.github.com>
2022-10-28 15:35:45 +0100

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=ywCO38fYbJ9Ehl5#
+export POSTGRES_HOST=alx-udacity-db.clv3deso9fxe.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=alx-udacity-bucket
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
